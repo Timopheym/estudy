fc1df3d23ae4ae9f04cba49f24c6ce1ff6febc5c
Timopheym <timopheym@gmail.com>
2013-11-10 03:44:38 +0400

    adding previews

--- app/models/infographic.rb
+++ app/models/infographic.rb
@@ -1,6 +1,6 @@
 class Infographic < ActiveRecord::Base
   # attr_accessible :title, :body
-  mount_uploader :src, InfograficUploader
+  #mount_uploader :src, InfograficUploader
   attr_accessible :id
   has_many  :categories, :through => :infographic_category
 end
